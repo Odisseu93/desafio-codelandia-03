e32078e3192606b8c92d7085177fd6b4211951ee
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-07 11:58:17 -0300

    feat: add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+			brand: '#6C63FF',
+			'brand-light': '#D7D4FF',
+	
+			/* dark scale */
+			dark: {
+				10: '#2A2A2A', /* 100% */
+				20: '#555555', /* 70% */
+				30: '#F9F9F9', /* 20% */
+				40: '#FFFFFF', /* 10% */
+			},
+	
+			/* color system */
+			system: {
+				message: '#2962FF',
+				success: '#0BB07B',
+				warning: '#FFCE52',
+				error: '#F03D3D',
+				info: '#E0E0E0',
+			}
+    },
     extend: {},
   },
   plugins: [],
